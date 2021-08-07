6e7e0b7508dd5ec6d15fb6c16d5f5611855dac88
JUsltop <jusltop@JUsltops-MacBook-Pro.local>
2021-08-07 18:09:33 +0800

    initial xcode project

---
+++ MyUIkit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyUIkit
+//
+//  Created by JUsltop on 7/8/2021.
+//  Copyright © 2021 JUsltop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onBtnPress(_ sender: Any) {
+        
+        print("btn pressed")
+        
+    }
+    
+}
+
